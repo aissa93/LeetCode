ebbe6be322d95630b69deaa788582758b4d6bcaa
aissa93 <42221102+aissa93@users.noreply.github.com>
2022-06-26 20:08:43 +0200

    Time: 259 ms, Memory: 45 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cs
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cs
@@ -4,8 +4,7 @@ public class Solution
     {
 
         int actualIndex =  1;
-        //0,0,1,1,1,2,2,3,3,4
-
+        
         for(int i = 1; i < nums.Length; i++)
         {
             if(nums[i] != nums[i-1])
