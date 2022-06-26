f31b5f08bead36a9e729001b6efab6c18c2f407c
aissa93 <42221102+aissa93@users.noreply.github.com>
2022-06-26 16:56:07 +0200

    Time: 148 ms, Memory: 43 MB - LeetHub

--- merge-sorted-array/merge-sorted-array.cs
+++ merge-sorted-array/merge-sorted-array.cs
@@ -2,29 +2,31 @@ public class Solution
 {
     public void Merge(int[] nums1, int m, int[] nums2, int n)
     { 
-        int i;
-        if(n>0)
+        int index_1 = m-1;
+        int index_2 = n-1;
+        int index_big = m + n -1;
+        
+        while(index_1 >= 0 && index_2 >= 0)
         {
-            for(i = nums1.Length -1; i >= 0 && n>0 && m>0;i--)
+            if(nums1[index_1] > nums2[index_2])
             {
-                if(nums2[n-1] > nums1[m-1])
-                {
-                    nums1[i] = nums2[n-1];
-                    n--;
-                }
-                else
-                {
-                    nums1[i] = nums1[m-1];
-                    m--;
-                
-                }
+                nums1[index_big] =  nums1[index_1];
+                index_1--;
             }
-            while(i >= 0 && n> 0)
+            else
             {
-                nums1[i] = nums2[n-1];
-                n--;
-                i--;
+                nums1[index_big] =  nums2[index_2];
+                index_2--;
+            }
+            index_big--;
+        }
+            while(index_2 >= 0)
+            {
+                nums1[index_big] = nums2[index_2];
+                index_1--;
+                index_2--;
+                index_big--;
             }
         }
-    }
+    
 }
